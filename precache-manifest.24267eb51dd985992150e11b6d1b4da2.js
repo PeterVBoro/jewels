0798d72613b0aba581c286b35499520f75e48cee
Peter Borodianski <pborodianski@ucdavis.edu>
2019-09-25 20:10:05 -0700

    Updates

---
+++ precache-manifest.24267eb51dd985992150e11b6d1b4da2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe573ee079d18a40424bdd445c7abd0",
+    "url": "/jewels/index.html"
+  },
+  {
+    "revision": "c927b38c06f517340ebf",
+    "url": "/jewels/static/css/main.c9f9297f.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/jewels/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "c927b38c06f517340ebf",
+    "url": "/jewels/static/js/main.f7f3a149.chunk.js"
+  },
+  {
+    "revision": "e71abc372706a0d44fc8",
+    "url": "/jewels/static/js/runtime~main.34f6a713.js"
+  }
+]);
